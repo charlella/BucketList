593379c2b70cf9367cb31249c63bc5b350afdf10
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-03-26 20:03:49 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by charlene hoareau on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
